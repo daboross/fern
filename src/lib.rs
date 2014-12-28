b375024b5e431c580c6147294d1eec019722224a
Dabo Ross <daboross@daboross.net>
2014-12-27 19:01:37 -0800

    Update docs

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@
 //! - Multiple outputs per logger - current options are to a file, or to stdout/stderr (or any combination of those)
 //! - Each output can have a Level configured, so you can output all log messages to a log file, and only have warnings and above show up in the console!
 //! - You can also define your own custom logging endpoints - have messages end up where you need them!
-//! - Thread-local logger storage. This allows for convenient <level>!() and log!() macros via the fern_macros package.
+//! - Thread-local logger storage. This allows for convenient debug/info/warning/severe!() and log!() macros via the fern_macros package.
 //!
 //! fern is still in development, and most features are experimental. The library is subject to change in non-backwards-compatible ways.
 //!
