3cef8c8bccccfc4e0b1671b1b697147801ef4cac
David Ross <daboross@daboross.net>
2018-01-02 10:52:50 -0800

    Make syslog example make more sense.

--- examples/syslog.rs
+++ examples/syslog.rs
@@ -5,7 +5,7 @@ extern crate syslog;
 
 fn setup_logging() -> Result<(), fern::InitError> {
     fern::Dispatch::new()
-        // by default only accept warning messages from libraries so as not to spam
+        // by default only accept warning messages so as not to spam
         .level(log::LevelFilter::Warn)
         // but accept Info if we explicitly mention it
         .level_for("explicit-syslog", log::LevelFilter::Info)
@@ -18,28 +18,22 @@ fn setup_logging() -> Result<(), fern::InitError> {
 fn main() {
     setup_logging().expect("failed to initialize logging.");
 
-    // Emulate a library we're using which has tons of debugging on the 'info' level.
-    info!(target: "overly-verbose-target", "hey, another library here, we're starting.");
-
+    // None of this will be shown in the syslog:
     for i in 0..5 {
         info!("executing section: {}", i);
 
         debug!("section {} 1/4 complete.", i);
 
-        info!(target: "overly-verbose-target", "completed operation.");
-
         debug!("section {} 1/2 complete.", i);
-
-        info!(target: "overly-verbose-target", "completed operation.");
-
-        info!(target: "overly-verbose-target", "completed operation.");
 
         debug!("section {} 3/4 complete.", i);
 
         info!("section {} completed!", i);
     }
 
-    info!(target: "explicit-syslog-info", "hello to the syslog! this is rust.");
+    // these two *will* show.
 
-    warn!(target: "overly-verbose-target", "AHHH something's on fire.");
+    info!(target: "explicit-syslog", "hello to the syslog! this is rust.");
+
+    warn!("AHHH something's on fire.");
 }
